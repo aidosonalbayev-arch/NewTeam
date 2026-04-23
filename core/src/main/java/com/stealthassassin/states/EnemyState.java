93f5d1e76c1bb0f776e534861a59148f1bd7f7be
Asylbek <asylbek.nurgalymuly06@gmail.com>
2026-04-24 00:44:48 +0500

    feat: add EnemyState interface for State pattern implementation

---
+++ core/src/main/java/com/stealthassassin/states/EnemyState.java
@@ -0,0 +1,8 @@
+﻿package com.stealthassassin.states;
+
+import com.stealthassassin.entities.enemies.Enemy;
+
+public interface EnemyState {
+    void handle(Enemy enemy, float delta);
+    String getStateName();
+}
