c40069140c122147334d8be8581c7e028442915b
Asylbek <asylbek.nurgalymuly06@gmail.com>
2026-04-28 11:13:24 +0500

    feat: implement Guard enemy with basic patrol behavior

---
+++ core/src/main/java/com/stealthassassin/entities/enemies/Guard.java
@@ -0,0 +1,8 @@
+package com.stealthassassin.entities.enemies;
+
+public class Guard extends Enemy {
+
+    public Guard(float x, float y) {
+        super(x, y, 50f, 100f, 150f, 15f, "Guard");
+    }
+}
